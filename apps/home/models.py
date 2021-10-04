7140a734018f7ac71fc5302a0fb5c7552ed061f8
CC Lee <alex112401@gmail.com>
2021-10-04 09:08:28 +0000

    model build

--- apps/home/models.py
+++ apps/home/models.py
@@ -8,3 +8,16 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class EventList(models.Model):
+    username = models.ForeignKey(User, on_delete = models.CASCADE)
+    eventname = models.CharField(max_length=50)
+    eventdate = models.DateField(null=True)
+    predtime = models.FloatField(max_length=50)
+    emerge = models.CharField(max_length=50)
+    iscomplete = models.BooleanField(default=False)
+    costtime = models.FloatField(null=True, blank=True)
+
+class Dailyfreetime(models.Model):
+    username = models.ForeignKey(User, on_delete = models.CASCADE)
+    timedate = models.DateField(null=True)
+    freetime = models.FloatField(max_length=50)
